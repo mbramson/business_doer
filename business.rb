45fe5da377b87aa978e19d42034809990b240763
Kelly McGowan <kellymmcgowan@gmail.com>
2017-03-15 18:36:18 -0400

    Open Source Stuff

--- business.rb
+++ business.rb
@@ -1,5 +1,11 @@
 class Business
   def perform
-    "Business"
+    "Super Nintendo Chalmers!"
+  end
+  def yes
+    "Santos L. Halper"
+  end
+  def no
+    "I saw this in a movie about a bus that had to speed around the city, keeping its speed over fifty, and if its speed dropped, it would explode! I think it was called “The Bus That Couldn’t Slow Down.”"
   end
 end
